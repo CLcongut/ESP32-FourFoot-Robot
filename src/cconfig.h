57fe173cbdec4a026a9a88b8e6b0dcadb02b6ea9
congut <clcongut@outlook.com>
2024-10-16 14:23:22 +0800

    基础框架搭建

--- src/cconfig.h
+++ src/cconfig.h
@@ -4,4 +4,4 @@
 #define wifi_PSWD "88888888"
 // this device ip is 192.168.31.71
 
-// #define UDPDEBUG
+#define UDPDEBUG
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,22 @@
 #include <Arduino.h>
-#include <BluetoothSerial.h>
 #include <WiFi.h>
 #include <FastLED.h>
 #include <U8g2lib.h>
 #include "motion.h"
 #include "cconfig.h"
 
-IPAddress remote_IP(192, 168, 31, 199);
-uint32_t remoteUdpPort = 8080;
+static IPAddress remote_IP(192, 168, 31, 199);
+static uint32_t remoteUdpPort = 8080;
 
-BluetoothSerial SerialBT;
+static WiFiUDP udp;
+static Motion motion;
+static CRGB leds[2];
+static U8G2_SSD1306_128X64_NONAME_F_HW_I2C oled(U8G2_R2);
 
-Motion motion;
-CRGB leds[2];
+static TaskHandle_t xMotionTask = NULL;
+static TaskHandle_t xOLEDTask = NULL;
+static TaskHandle_t xWS2812Task = NULL;
 
-#ifdef UDPDEBUG
-WiFiUDP udp;
 void udp_control(void *param)
 {
   WiFi.mode(WIFI_STA);
@@ -37,6 +38,7 @@ void udp_control(void *param)
       char val[packetSize];
       udp.read(val, packetSize);
       String recvStr = String(val);
+#ifdef UDPDEBUG
       Serial.print("UDP Received:");
       Serial.println(recvStr);
 
@@ -69,50 +71,73 @@ void udp_control(void *param)
           motion.setFrequency(cmdFreq);
         }
       }
+#endif
+      if (recvStr.startsWith("#"))
+      {
+        String notifyValueStr = recvStr.substring(recvStr.indexOf("#") + 1);
+        uint32_t notifyValue = notifyValueStr.toInt();
+        xTaskNotify(xMotionTask, notifyValue, eSetValueWithOverwrite);
+      }
     }
     vTaskDelay(1);
   }
 }
-#endif
 
-void bluetooth_control(void *param)
+void motion_task(void *param)
 {
-  SerialBT.begin("ESP32-FFR");
+  motion.begin();
+  uint32_t ulNotifyValue = 0;
   for (;;)
   {
-    if (SerialBT.available())
-    {
-      String recvStr = SerialBT.readString();
-      Serial.print("BT Received:");
-      Serial.println(recvStr);
-      int cmdNum = recvStr.toInt();
-      motion.controlNumber(cmdNum);
-    }
+    xTaskNotifyWait(0, 0xff, &ulNotifyValue, portMAX_DELAY);
+    motion.controlNumber(ulNotifyValue);
     vTaskDelay(1);
+  }
+}
+
+void oled_task(void *param)
+{
+  oled.begin();
+  oled.clearBuffer();
+  oled.setFont(u8g2_font_crox2h_tr);
+  oled.drawStr(22, 20, "Hello, ESP32!");
+  oled.sendBuffer();
+
+  for (;;)
+  {
+    vTaskDelay(1);
+  }
+}
+
+void ws2812_task(void *param)
+{
+  FastLED.addLeds<WS2812B, 5, GRB>(leds, 2);
+  for (;;)
+  {
+    // Turn the LED on, then pause
+    leds[0] = CRGB::Blue;
+    leds[1] = CRGB::Blue;
+    FastLED.show();
+    vTaskDelay(500);
+    // Now turn the LED off, then pause
+    leds[0] = CRGB::Black;
+    leds[1] = CRGB::Black;
+    FastLED.show();
+    vTaskDelay(500);
   }
 }
 
 void setup()
 {
   Serial.begin(115200);
-  motion.begin();
-  FastLED.addLeds<WS2812B, 5, GRB>(leds, 2);
-#ifdef UDPDEBUG
+
   xTaskCreatePinnedToCore(udp_control, "udp_control", 2048, NULL, 2, NULL, 0);
-#endif
-  xTaskCreatePinnedToCore(bluetooth_control, "bluetooth_control", 2048, NULL, 2, NULL, 0);
+  xTaskCreate(motion_task, "motion_task", 2048, NULL, 2, &xMotionTask);
+  xTaskCreate(oled_task, "oled_task", 2048, NULL, 2, &xOLEDTask);
+  xTaskCreate(ws2812_task, "ws2812_task", 1024, NULL, 2, &xWS2812Task);
 }
 
 void loop()
 {
-  // Turn the LED on, then pause
-  leds[0] = CRGB::Blue;
-  leds[1] = CRGB::Blue;
-  FastLED.show();
-  vTaskDelay(500);
-  // Now turn the LED off, then pause
-  leds[0] = CRGB::Black;
-  leds[1] = CRGB::Black;
-  FastLED.show();
-  vTaskDelay(500);
+  vTaskDelete(NULL);
 }
